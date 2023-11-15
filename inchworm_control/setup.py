29788da5e5690e0e043839a204cc6cbd99d9404b
Cambria <cdpomeranz@wpi.edu>
2023-11-15 18:40:35 -0500

    adding inchworm_control to src

---
+++ inchworm_control/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'inchworm_control'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motor_controller = inchworm_control.motor_controller:main'
+        ],
+    },
+)
+
